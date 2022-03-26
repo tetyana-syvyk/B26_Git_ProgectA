f1e602d524502653339e76273eeefcb5c01e1d94
Tetyana A Syvyk <dr.syvyk@hotmail.com>
2022-03-26 15:38:13 -0500

    updated JamesBondA

--- src/Day_0/JamesBondA.java
+++ src/Day_0/JamesBondA.java
@@ -1,5 +1,7 @@
-package day0
-public class JamesBondA{
+package Day0;
+public class JamesBondA {
 
-public static void main(String[] args) {
-System.out.println("hulibaka James Bond")
+    public static void main(String[] args) {
+        System.out.println("hulibaka James Bond");
+    }
+}

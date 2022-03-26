8b6cdd99133a70ebdb54989b7b2625a15c5e688b
Tetyana A Syvyk <dr.syvyk@hotmail.com>
2022-03-26 15:12:43 -0500

    Added First Class added code

--- src/Day_0/First.java
+++ src/Day_0/First.java
@@ -1,8 +1,10 @@
 package Day_0;
 
 public class First {
-    //TODO: Work on this tomorrow
-    //add some cool code,
+    public static void main(String[] args) {
+        System.out.println("java");
+        System.out.println("Working...");
+    }
     //extra line 1
     //extra line 2
     //extra line 3
